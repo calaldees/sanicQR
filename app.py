d63f4fc84fa9173dac2742fd9918b9d9cd526326
Allan Callaghan <allan.callaghan@global.com>
2025-04-08 18:51:47 +0100

    ruff lint

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import qrcode
 import sanic
 from qrcode.image.pure import PyPNGImage
 
-app = sanic.Sanic('QR')
+app = sanic.Sanic("QR")
 
 
-@app.get('/')
+@app.get("/")
 async def qr(request):
-    data = request.args.get('data', '')
+    data = request.args.get("data", "")
     buffer = BytesIO()
     qrcode.make(data, image_factory=PyPNGImage).save(buffer)
-    return sanic.response.raw(buffer.getvalue(), content_type='image/png')
+    return sanic.response.raw(buffer.getvalue(), content_type="image/png")
