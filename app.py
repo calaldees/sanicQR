24bd2dfc04e84ee6d9a8a04be34a76a1a51d2355
Allan Callaghan <allan.callaghan@global.com>
2025-04-25 10:41:15 +0100

    rename `get_` to `build_`

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import base64
 from io import BytesIO
 
-import qrcode
+import qrcode  # type: ignore[import-untyped]
 import sanic
-from qrcode.image.pure import PyPNGImage
+from qrcode.image.pure import PyPNGImage  # type: ignore[import-untyped]
 
 app = sanic.Sanic("QR")
 
 
-def get_qr_png_bytes(data: str) -> bytes:
+def build_qr_png_bytes(data: str) -> bytes:
     r"""
-    >>> png_bytes = get_qr_png_bytes('abc')
+    >>> png_bytes = build_qr_png_bytes('abc')
     >>> png_bytes.startswith(b'\x89PNG\r\n')
     True
     """
@@ -19,13 +19,13 @@ def get_qr_png_bytes(data: str) -> bytes:
     return buffer.getvalue()
 
 
-def get_qr_png_base64(data: str) -> bytes:
+def build_qr_png_base64(data: str) -> bytes:
     """
-    >>> get_qr_png_base64('abc')
+    >>> build_qr_png_base64('abc')
     b'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAAEiAQAAAAB1xeIbAAABDUlEQVR4nO3YQQ6DIBCF4Uk8AEfi6j1SD2BCYWAUbahdOCRtfhalwufqBUaQ9EV7CAqFQqH+QklrS5JYHuPTRiLKW+lDehaVf/RfN4FyVSWSAkSC+S0w1ByVRzaPmqv6rFBzlHaakK2ObgLlqbqKvMX0sW6j7lIXDeWochCrZRJW0SXS9iiUtypzDRht36QoZ5V7+xCqIGlM62lnQnmolGxNSLAiUVbHMSGUi6pnXh3u96hTQigPZc0S2l9C+SrLZKm1uc0FexXlqaJ2pxu3t4RQLup441YKtAWGmqbahY+0gjBICOWgdE3sh4JBQqg7lXb1xm14CkC5qL0i16+hwRkZdb+6aCgUCoX6efUCVPGJPFhcrgoAAAAASUVORK5CYII='
     """
     return b"data:image/png;base64," + base64.encodebytes(
-        get_qr_png_bytes(data)
+        build_qr_png_bytes(data)
     ).replace(b"\n", b"")
 
 
@@ -37,10 +37,10 @@ async def root(request: sanic.Request) -> sanic.HTTPResponse:
 @app.get("/qr_png")
 async def qr_png(request: sanic.Request) -> sanic.HTTPResponse:
     data = request.args.get("data", "")
-    return sanic.response.raw(get_qr_png_bytes(data), content_type="image/png")
+    return sanic.response.raw(build_qr_png_bytes(data), content_type="image/png")
 
 
 @app.get("/qr_png_base64")
 async def qr_png_base64(request: sanic.Request) -> sanic.HTTPResponse:
     data = request.args.get("data", "")
-    return sanic.response.raw(get_qr_png_base64(data), content_type="text/plain")
+    return sanic.response.raw(build_qr_png_base64(data), content_type="text/plain")
